bdcb76452f0b281b95c1203eed1bd562ea71601b
rei-rala <ramonirala@outlook.com>
2023-08-25 16:44:52 -0300

    fixing types in tsconfig try#2

--- src/types/mui.d.ts
+++ src/types/mui.d.ts
@@ -1,3 +1,5 @@
+import Typography from "@mui/material/Typography";
+
 // Overriding variants for material UI components with module augmentation (TypeScript)
 // ref: https://mui.com/material-ui/customization/theme-components/
 declare module "@mui/material/Typography" {
