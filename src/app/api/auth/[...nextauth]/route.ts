47ff8c409c01d81a2a6e8a884838dba10fd17621
rei-rala <ramonirala@outlook.com>
2024-07-09 18:29:04 -0300

    Restaurando nextauth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,15 @@
+import type { NextRequest, NextResponse } from "next/server";
 import NextAuth from "next-auth";
 
 import authOptions from "@/libs/nextAuth";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function auth(req: NextApiRequest, res: NextApiResponse) {
-  const handler = await NextAuth(req, res, authOptions);
+async function auth(req: NextRequest, res: NextResponse) {
+  const handler = await NextAuth(
+    req as unknown as NextApiRequest, // WTF?
+    res as unknown as NextApiResponse, // WTF?
+    authOptions
+  );
 
   return handler;
 }
