f79e3abc1d6e33e5c6ae5451da8c96acee00bf36
rei-rala <ramonirala@outlook.com>
2024-07-12 04:21:20 -0300

    y por que esto no sale en el linter

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -1,7 +1,4 @@
-export function removeKeysFromObject<T>(
-  object: T,
-  keys: readonly string[]
-): Omit<T, keyof typeof keys> {
+export function removeKeysFromObject<T>(object: T, keys: string[]): Omit<T, keyof typeof keys> {
   for (let key of keys) {
     delete (object as Partial<T>)[key as keyof T];
   }
