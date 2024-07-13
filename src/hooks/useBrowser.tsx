70eb01af95d702c3b166ecf1a28f4bfbfa95d42e
rei-rala <ramonirala@outlook.com>
2024-07-13 18:03:12 -0300

    escuchando el scroll en useBrowser

--- src/hooks/useBrowser.tsx
+++ src/hooks/useBrowser.tsx
@@ -4,6 +4,18 @@ import React, { useState, useEffect } from "react";
 
 export const useBrowser = () => {
   const [isDarkThemed, setIsDarkThemed] = useState(false);
+  const [scrollX, setScrollX] = useState(0);
+  const [scrollY, setScrollY] = useState(0);
+
+  const scroll = {
+    x: scrollX,
+    y: scrollY,
+  };
+
+  const scrollPercentage = {
+    x: Math.max(0, scrollX / (document.documentElement.scrollWidth - window.innerWidth)),
+    y: Math.max(0, scrollY / (document.documentElement.scrollHeight - window.innerHeight)),
+  };
 
   useEffect(() => {
     let colorSchemeQuery: MediaQueryList;
@@ -23,5 +35,16 @@ export const useBrowser = () => {
     return () => colorSchemeQuery.removeEventListener("change", handleColorSchemeChange);
   }, []);
 
-  return { isDarkThemed };
+  useEffect(() => {
+    function handleScroll() {
+      setScrollX(window.scrollX);
+      setScrollY(window.scrollY);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return { isDarkThemed, scrollPercentage, scroll };
 };
