17ba4bfa9419d7152b20d91991b59b47bf071ab9
rei-rala <ramonirala@outlook.com>
2024-07-24 21:18:31 -0300

    opengraph kinda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,13 @@ import "./globals.css";
 import type { Metadata } from "next/types";
 
 import Providers from "./providers";
-import { BRAND } from "@/constants";
+import { BRAND, NEXTAUTH_URL } from "@/constants";
 
 export const metadata: Metadata = {
-  title: BRAND,
-  description: `Inicia sesión en ${BRAND}`,
+  title: `${BRAND} - crea y comparte enlaces cortos`,
+  description: `${BRAND} es una aplicación que permite crear enlaces cortos y perfiles con enlaces personalizados para compartir tus contenidos de manera fácil y rápida.`,
+  keywords: "enlaces cortos, acortador de enlaces, compartir enlaces, KORT.AR " + BRAND,
+  authors: [{ name: "KORT.AR" }],
   icons: [
     {
       rel: "icon",
@@ -14,6 +16,22 @@ export const metadata: Metadata = {
       url: "/favicon/kortar.png",
     },
   ],
+  openGraph: {
+    title: `${BRAND} - crea y comparte enlaces cortos`,
+    description: `${BRAND} es una aplicación que permite crear enlaces cortos y perfiles con enlaces personalizados para compartir tus contenidos de manera fácil y rápida.`,
+    images: "/@kortar.png",
+    url: NEXTAUTH_URL,
+    type: "website",
+    locale: "es_ES",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${BRAND} - crea y comparte enlaces cortos`,
+    description: `${BRAND} es una aplicación que permite crear enlaces cortos y perfiles con enlaces personalizados para compartir tus contenidos de manera fácil y rápida.`,
+    images: "/@kortar.png",
+    site: "@_kortar",
+    creator: "@_kortar",
+  },
 };
 
 function RootLayout({ children }: { readonly children: React.ReactNode }) {
