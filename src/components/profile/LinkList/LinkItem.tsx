cb4a8e1f72aedc5b4ecf478776a897f516cea237
Ramon Irala <rirala@garantizar.com.ar>
2024-07-25 17:59:20 -0300

    profile page: link list update

--- src/components/profile/LinkList/LinkItem.tsx
+++ src/components/profile/LinkList/LinkItem.tsx
@@ -1,15 +1,37 @@
+"use client";
+
 import { NEXTAUTH_URL } from "@/constants";
-import { Link } from "@mui/material";
+import { Button, Link } from "@mui/material";
 import Paper from "@mui/material/Paper/Paper";
 import Typography from "@mui/material/Typography/Typography";
 
+import OfflineShareIcon from "@mui/icons-material/OfflineShare";
+
 const LinkItem: ExtendedComponent<{ link: RedirectLink }> = ({ link }) => {
   return (
-    <Link href={`${NEXTAUTH_URL}/${link.from}?skip=true`} target="_blank">
-      <Paper elevation={2} sx={{ padding: "0.5rem", textAlign: "center" }}>
+    <Paper
+      elevation={2}
+      sx={{
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+        padding: "0.5rem",
+        textAlign: "center",
+      }}
+    >
+      <Link href={`${NEXTAUTH_URL}/${link.from}?skip=true`} target="_blank" sx={{ flex: 1 }}>
         <Typography variant="button">{link.alias || link.to}</Typography>
-      </Paper>
-    </Link>
+      </Link>
+      <Button
+        sx={{ position: "absolute", right: 0 }}
+        onClick={(e) => {
+          e.stopPropagation();
+          console.log(e);
+        }}
+      >
+        <OfflineShareIcon />
+      </Button>
+    </Paper>
   );
 };
 
